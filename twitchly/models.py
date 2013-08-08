7152b0cb5fbf0b3a635fd7cbcef0db4dcad1ac9e
jrubio <jose@twitchly.com>
2013-08-08 15:31:56 -0500

    anything left

--- twitchly/models.py
+++ twitchly/models.py
@@ -28,7 +28,20 @@ class Circle(models.Model):
     type = models.CharField(max_length=100)
     description = models.TextField()
     users = models.ManyToManyField(User)
+    leader = models.ForeignKey(User,related_name="leader_user")
+    repeat_type = models.CharField(max_length=25)
+    initial_date = models.DateTimeField('initial_date')
+    price = models.FloatField()
     
- 
     def __str__(self):
         return self.name + ":" + self.type
+    
+class CircleMeeting(models.Model):
+    circle = models.ForeignKey(Circle)
+    meeting_date = models.DateTimeField('meeting_date')
+    attending_users = models.ManyToManyField(User)
+    price_override = models.FloatField()
+    
+    def __str__(self):
+        return "Meeting for"+ self.circle.name + "on" + self.meeting_date
+    
